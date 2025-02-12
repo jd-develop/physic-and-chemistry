03c69540353840908a86312be9a11682226d539e
Jean Dubois <jd-dev@laposte.net>
2025-02-12 12:57:44 +0100

    add TP13 montecarlo

---
+++ MP2I/TP13/montecarlo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# Méthode de Monte-Carlo pour calculer la précision de la vitesse des ultrasons
+# mesurée
+
+import numpy as np
+
+N = 100000
+# valeurs numériques
+L = 8.8e-2
+precision_L = 4e-3
+freq = 40e3  # 40 kHz
+precision_freq = 100
+
+L_mc = np.random.normal(L, precision_L/np.sqrt(3), N) # + precision_L * np.random.uniform(-1, 1, N)
+f_mc = np.random.normal(freq, precision_freq/np.sqrt(3), N)
+v_mc = (L_mc/10)*(f_mc)
+
+moyenne = np.average(v_mc)
+incertitude = np.std(v_mc)
+print(f"moyenne: {moyenne}, incertitude: {incertitude}")
+
