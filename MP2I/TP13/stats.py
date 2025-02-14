fea76de07a4ac8f76a8afd6b1fb164029531daa2
ebdm13 <elie.bellotdesminieres@gmail.com>
2025-02-14 13:48:38 +0100

    changements mineurs

--- MP2I/TP13/stats.py
+++ MP2I/TP13/stats.py
@@ -11,13 +11,14 @@ i = np.array([0.74, 0.5, 0.3])*(1e-2)  # centimètres
 
 a_di, b_di = np.polyfit(1/d, i, 1)
 
-plt.plot(d, i, "ro")
-plt.plot(np.linspace(200e-6, 500e-6, 100), a_di * 1/np.linspace(200e-6, 500e-6, 100) + b_di)
-plt.text(250e-6, 7e-3, f"i = {a_di}/d + {b_di}")
-plt.xlabel("d en m")
-plt.ylabel("i en m")
-plt.grid(True)
-plt.show()
+# plt.plot(1/d, i, "ro")
+# x = 1 / np.linspace(200e-6, 500e-6, 100)
+# plt.plot(x, a_di * x + b_di)
+# plt.text(2000, 0.007, f"i = {round(a_di,8)}/d + {round(b_di, 8)}")
+# plt.xlabel("1/d en 1/m")
+# plt.ylabel("i en m")
+# plt.grid(True)
+# plt.show()
 
 # i en fonction de D
 # avec d = 200 μm
@@ -28,7 +29,7 @@ a_Di, b_Di = np.polyfit(D, i, 1)
 
 plt.plot(D, i, "ro")
 plt.plot(np.linspace(30.5e-2, 242.5e-2, 500), a_Di * np.linspace(30.5e-2, 242.5e-2, 500) + b_Di)
-plt.text(0.3, 7e-3, f"i = {a_Di}D + {b_Di}")
+plt.text(0.3, 7e-3, f"i = {round(a_Di, 6)}D - {abs(round(b_Di, 6))}")
 plt.xlabel("D en m")
 plt.ylabel("i en m")
 plt.grid(True)
