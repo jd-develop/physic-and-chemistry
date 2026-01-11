03e8a4a92351ad63263feee41311119613096d31
Jean Dubois <jd-dev@laposte.net>
2026-01-11 11:50:41 +0100

    TP interférences

---
+++ MPI/TP11/reglin1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# type: ignore
+import numpy as np
+from matplotlib import pyplot as plt
+
+d = [30, 40, 50, 60, 70, 80]  # centimètres
+r = np.array([0.45+0.55, 0.6+0.7, 0.75+0.9, 0.9+1.1, 1.0+1.3, 1.15+1.5])/2  # largeur tache centrale, centimètres
+u_r = (1/10) / (2*np.sqrt(3))
+λ = 6.50e-5  # centimètres
+largeur_fente_constructeur = 40
+
+pente_list = []
+b_list = []
+
+for i in range(1000):
+    new_r = r + np.random.uniform(-1, 1, len(r))*u_r
+    pfit = np.polyfit(d, new_r, 1)
+    a, b = pfit[0], pfit[1]
+    pente_list.append(a)
+    b_list.append(b)
+
+pente, u_p = np.mean(pente_list), np.std(pente_list)
+# print(pente, u_p)
+x=np.linspace(min(d), max(d), 1000)
+
+largeur_fente = (λ/pente)*(10**4)
+u_a = (a * u_p/pente)*(10**4)
+print(f"a = {largeur_fente:.2f}±{u_a:.2f} μm")
+
+z_score = abs(largeur_fente_constructeur-largeur_fente)/u_a
+print(f"Z-score : {z_score}")
+
+plt.errorbar(d, r, xerr=0, yerr=u_r, fmt='.')
+plt.plot(d, r, '.')
+plt.plot(x, pente*x)
+plt.show()
+
---
+++ MPI/TP11/reglin2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# type: ignore
+import numpy as np
+from matplotlib import pyplot as plt
+
+τ = 3.96   # ms, interfrange
+u_τ = np.sqrt(2/3) * 0.5  # largeur de l’intervalle de confiance : 1 ms
+I = 9  # nombre d’interfranges
+
+# total de la barette
+d = 2048 * 14  # micromètres
+T = 6.58  # ms
+
+D = (190.8-35) * 10  # millimètres
+λ = 650e-6  # millimètres
+
+i = ((d*τ/T)/I)/1000  # largeur d’une interfrange
+u_i = i * u_τ/τ
+print(f"interfrange i = {i:.2f} ± {u_i:.2f} mm")  # millimètres
+
+b = λ*D/i  # millimètres
+u_b = b * u_τ/τ
+print(f"écart entre les fentes b = {b:.2f} ± {u_b:.2f} mm")
+
+b_constructeur = 0.5  # millimètres
+z_score = abs(b_constructeur-b)/u_b
+print(f"Z-score : {z_score:.2f}")
+
